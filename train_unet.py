2cff72dac90c93fe23f9f4063687f777f84cc326
Riyank Mukhopadhyay <33059184+Riyank7@users.noreply.github.com>
2019-06-02 21:19:25 +0530

    Update train_unet.py

--- train_unet.py
+++ train_unet.py
@@ -20,12 +20,12 @@ def normalize(img):
 N_BANDS = 8
 N_CLASSES = 5  # buildings, roads, trees, crops and water
 CLASS_WEIGHTS = [0.2, 0.3, 0.1, 0.1, 0.3]
-N_EPOCHS = 5
+N_EPOCHS = 10
 UPCONV = True
 PATCH_SZ = 160   # should divide by 16
 BATCH_SIZE = 50
-TRAIN_SZ = 40  # train size
-VAL_SZ = 10    # validation size
+TRAIN_SZ = 400  # train size
+VAL_SZ = 100    # validation size
 
 
 def get_model():
